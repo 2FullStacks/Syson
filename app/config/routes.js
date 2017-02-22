65b518619572adefe45a3c191a7620cd9f613787
felansu <gaferran@gmail.com>
2017-02-21 23:45:37 -0300

    Resolvido problema md-sidemenu-button ng-click

---
+++ app/config/routes.js
@@ -0,0 +1,19 @@
+(function () {
+	angular.module('app')
+		.config(config);
+
+	/* @ngInject */
+	function config($stateProvider) {
+
+        const dashboard = {
+            name: 'dashboard',
+            url: '/dashboard',
+            templateUrl: '../app/views/dashboard/dashboard.html'
+        };
+
+
+		$stateProvider
+    		.state('dashboard', dashboard);
+
+	}
+})();
